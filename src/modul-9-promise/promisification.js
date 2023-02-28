9110ad0ceedd673f51f4aecd8408508ff48acbed
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-02-28 23:58:39 +0200

    add pokemons

--- src/modul-9-promise/promisification.js
+++ src/modul-9-promise/promisification.js
@@ -1,22 +1,5 @@
-// const makeOrder = (dish)=> {
-//     const DELAY = 1000;
 
-//     return new Promise((resolve, reject)=> {
-//     const passed = Math.random() > 0.5;
-//     console.log('passed -->', passed);
-
-//     setTimeout(()=> {
-//         if(passed) {
-//             resolve('Your order have cooked');
-//         } else {
-//             reject("Sorry! We cannot cook your order")
-//         }
-        
-//     }, DELAY)
-//     })
-  
-// };
-
+// 1-й Пример
 // makeOrder("KEBAB") 
 // .then((result)=> {
 //     console.log(result)
@@ -28,24 +11,43 @@
 //     console.log('This log will appear anytimes')
 // })
 
-
+//2-й Пример
 // Pokemons from https://pokeapi.co/
 
-function fetchPokemonById(id) {
-  return  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-.then(r => r.json())
+// function fetchPokemonById(id) {
+//   return  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+// .then(r => r.json())
+// };
+
+// function onFetchSuccess(pokemon) {
+//     console.log('OnFetchSuccess --->')
+//     console.log('pokemon',pokemon);
+// };
+
+// function onFetchError(error) {
+//     console.log('onFetchError --->');
+//     console.log('error',error);
+// };
+
+// fetchPokemonById(1).then(onFetchSuccess).catch(onFetchError);
+// fetchPokemonById(2).then(onFetchSuccess).catch(onFetchError);
+// fetchPokemonById(3).then(onFetchSuccess).catch(onFetchError);
+
+// 3-й Пример
+
+const makePromise = () => {
+    return new Promise((resolve,reject)=> {
+        const passed = Math.random() > 0.5;
+        setTimeout(() => {
+            if(passed) {
+                resolve("Это метод промиса--resolve");
+            }
+            reject("Это метод промиса--reject")    
+        }, 2000);
+        
+    })
 };
 
-fetchPokemonById(1).then(onFetchSuccess).catch(onFetchError);
-// fetchPokemonById(2);
-// fetchPokemonById(3);
-
-function onFetchSuccess(pokemon) {
-    console.log('OnFetchSuccess --->')
-    console.log('pokemon',pokemon);
-};
-
-function onFetchError(error) {
-    console.log('onFetchError --->');
-    console.log('error',error);
-}
+makePromise()
+.then(result => console.log('result',result))
+.catch(error => console.log('error',error))
