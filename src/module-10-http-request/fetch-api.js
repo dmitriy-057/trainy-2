2ba82b5fc6cb6813623ff231750c8223335170af
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-03-15 23:56:44 +0200

    add fetchPokemon

--- src/module-10-http-request/fetch-api.js
+++ src/module-10-http-request/fetch-api.js
@@ -1,18 +1,49 @@
-// import pokemonCard from 'pokemon-card.hbs';
+const refs = {
+    cardContainer: document.querySelector(".card-container"),
+    searchForm: document.querySelector('.js-search-form'),
 
-import {markup} from "../module-10-http-request/pokemon-card.js";
+};
 
-const r = fetch("https://pokeapi.co/api/v2/pokemon/4/");
-console.log(r);
-r.then(response => {
-    console.log(response.json);
+refs.searchForm.addEventListener('submit', ((e) => {
+    e.preventDefault();
+    console.log('sdfvfvbb')
+    })
+);
+
+// function onSearch(e) {
+//     e.preventDefault();
+//     console.log('submit event')
+
+// }
+
+fetchPokemon(5)
+.then(pokemon => renderPokemonCard(pokemon))
+.catch(error => console.log(error))
+
+
+function fetchPokemon(pokemonId) {
+// возвращает промис
+return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+.then(response => {
+// распарсивает данные которые приходят с бэкэнда
     return response.json()
 })
-.then(pokemon => {
-    console.log('pokemon',pokemon)
-    const createMarkup = markup(pokemon);
-    console.log(createMarkup);
-})
-.catch(error => {
-    console.log(error)
-})
+};
+
+function renderPokemonCard(pokemon) {
+    console.log('pokemon',pokemon);
+
+    const markup = `
+    <div class="card">
+    <div class="image-card-top">
+    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" width=200 height=200>
+    </div>
+    <div class="card-body">
+        <h2 class="card-title">Имя:${pokemon.name}</h2>
+        <p class="card-text">Вес:${pokemon.weight}</p>
+        <p class="card-text">Рост:${pokemon.height}</p>
+    </div>
+</div>`
+    refs.cardContainer.innerHTML = markup;
+  
+}
